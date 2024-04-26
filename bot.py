e0c9444923591e21f4b3a3e1d4ddb560e1e2c9d7
chatRSC <146732694+chatrsc07@users.noreply.github.com>
2024-04-27 08:18:56 +1000

    Megumin

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from ..Cow import config
 import random
 async def send_message(message, user_message, is_private):
     try:
-        response = "Hello"
+        response = "Hellog"
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
